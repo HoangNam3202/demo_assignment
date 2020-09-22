2eb1192e9d4865ada784f11d6b703df9c48d7f2d
Tuan0304 <tranminhshuan0304@gmail.com>
2020-09-22 15:37:39 +0700

    dwqdqw

--- NotificationDesktop/app/src/main/java/com/example/notificationdesktop/MainActivity.java
+++ NotificationDesktop/app/src/main/java/com/example/notificationdesktop/MainActivity.java
@@ -66,9 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "dwqdqwdqwdqw", Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "dwqdwqd", Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "ble", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "dwqdwqd", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "dw", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "dw1", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
