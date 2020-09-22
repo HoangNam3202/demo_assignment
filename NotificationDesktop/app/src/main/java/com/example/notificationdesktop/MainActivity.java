3c0c0061b84812f807a30228bd059ac8c53eaf17
HoangNam1101 <namhhpk01637@fpt.edu.vn>
2020-09-22 15:32:30 +0700

    Merge branch 'master' of E:\demo_assignment with conflicts.

--- NotificationDesktop/app/src/main/java/com/example/notificationdesktop/MainActivity.java
+++ NotificationDesktop/app/src/main/java/com/example/notificationdesktop/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "dwqdwqd", Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "ble", Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "dwqdwqd", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "nam", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "dw", Toast.LENGTH_SHORT).show();
             }
         });
     }
